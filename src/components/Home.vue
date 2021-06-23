4b75507864ab59997d6ef4bc7c4aaa09046d2df0
xiaozhan93 <873633473@qq.com>
2021-06-24 02:18:21 +0800

    22

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,7 +13,7 @@
     <!-- 页面主题区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
         <!-- active-text-color 激活的文本的颜色 -->
@@ -24,6 +24,8 @@
           :unique-opened="true"
           :collapse="isCollapse"
           :collapse-transition="false"
+          :router="true"
+          :default-active="activePath"
         >
           <!-- 一级菜单 -->
           <!-- :index只接受字符串,item却是一个数值,所以拼接'' -->
@@ -38,9 +40,10 @@
 
             <!-- 二级菜单 -->
             <el-menu-item
-              :index="subItem.id + ''"
+              :index="'/' + subItem.path"
               v-for="subItem in item.children"
               :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
             >
               <template slot="title">
                 <!-- 图标 -->
@@ -53,7 +56,9 @@
         </el-menu>
       </el-aside>
       <!-- 右侧内容主体 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view />
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -72,12 +77,15 @@ export default {
         "102": "iconfont icon-detail-fill",
         "145": "iconfont icon-fund-fill",
       },
-       // 是否折叠
-      isCollapse:false
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: "",
     }
   },
   created() {
     this.getMenuList()
+    this.activePath = window.sessionStorage.getItem("activePath")
   },
   methods: {
     logout() {
@@ -85,19 +93,24 @@ export default {
       window.sessionStorage.clear()
       //重定向登录页面
       this.$router.push("/login")
-     
     },
 
     // 获取所有的菜单
     async getMenuList() {
       const { data: res } = await this.$http.get("menus")
-      console.log(res)
+      // console.log(res)
       if (res.meta.status !== 200) return this.$message.err(res.meta.msg)
       this.menulist = res.data
     },
     // 点击按钮 切换菜单的折叠与展开
     toggleCollapse() {
       this.isCollapse = !this.isCollapse
+    },
+
+    //保存激活链接的状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem("activePath", activePath)
+      this.activePath = activePath
     },
   },
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,6 +4,13 @@ import Vue from "vue"
 import { Button, Form, FormItem, Input, Message,Container,Header,Aside,Main,Menu,
     Submenu,
     MenuItem, 
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,Row,Col,
+    Table,
+    TableColumn,Switch,Tooltip,
+    Pagination,
+    Dialog
 
 } from "element-ui"
 
@@ -18,6 +25,17 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 
 // 把Message挂载Vue原型对象上
 // 这样每个组件都可以通过 this.$message 访问
