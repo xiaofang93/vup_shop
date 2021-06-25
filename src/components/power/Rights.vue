0d39e9c96fe4a103d632d613c3c8cb4a768c19f1
xiaozhan93 <873633473@qq.com>
2021-06-25 21:44:55 +0800

    001

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      nihao
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 列表权限
+      rightList: [],
+    }
+  },
+  created() {
+    //   获取所有的权限
+    this.getRightList()
+  },
+  methods: {
+    //获取权限列表
+    async getRightList() {
+      const{ data: res} =await this.$http.get("rights/list")
+      if(res.meta.status!==200) {
+return this.$message.error("获取Q")
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Login from "../components/Login.vue"
 import Home from "../components/Home.vue"
 import Welcome from "../components/Welcome.vue"
 import Users from "../components/user/Users.vue"
+import Rights from "../components/power/Rights.vue"
+
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -19,6 +22,7 @@ const routes = [
     children: [
       { path: "/welcome", component: Welcome },
       { path: "/users", component: Users },
+      { path: "/rights",component: Rights}
     ],
   },
 ]
